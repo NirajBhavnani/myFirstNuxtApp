e3d86c3f447ca3c6fb5605a4a3e3518041903963
nbhavna <niraj.bhavnani@morningstar.com>
2022-09-03 20:35:47 +0530

    Project setup

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,6 @@
+// for dynamic routing, we start the name with an underscore(_)
+<template>
+  <!-- Nuxt.js looks different but uses vue-router bts -->
+  <!-- Hence, we can use all the default vue-router features -->
+  <h3>Single user with id: {{ $route.params.id }}</h3>
+</template>
---
+++ pages/users/index.vue
@@ -0,0 +1,5 @@
+// In nuxt.js there's no router config file
+// we just create a folder in pages(root) and name the file as index.vue
+<template>
+  <h3>Users view</h3>
+</template>
